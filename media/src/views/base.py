f4c8aa5d9a79c8d0d6527db388699a68b17ea891
dyleeeeeeee <dyleexoxo@icloud.com>
2025-05-03 18:23:51 -0500

    refactor(base): Remove commented-out upload method and associated logic

--- media/src/views/base.py
+++ media/src/views/base.py
@@ -88,46 +88,6 @@ class BaseView(QuartClassful):
             status_code,
         )
 
-    # @route("/media/upload", methods=["GET", "POST"])
-    # @jwt_required
-    # async def upload(self):
-    #     """Upload a media type to our GCP Bucket"""
-    #     ...
-    #     file: werkzeug.datastructures.FileStorage = (await request.files).get("file")
-
-    #     # Get the data and attach fields
-    #     data = await request.form
-    #     data = data.to_dict()
-    #     data["creator"] = get_jwt_identity()
-    #     data["filename"] = file.filename
-
-    #     # Upload to GCP
-    #     file.stream.seek(0) # Reset the stream position
-    #     await obs.put_async(self.OBS_STORE, file.filename, file.stream)
-
-    #     # data["url"] = await obs.sign_async(
-    #     #     self.OBS_STORE, "GET", file.filename, timedelta(days=1)
-    #     # )
-
-    #     # upload to GCP
-    #     # blob = self.bucket.blob(file.filename)
-    #     # blob.upload_from_file(
-    #     #     file.stream,
-    #     #     content_type=file.content_type or "application/octet-stream",
-    #     #     rewind=True,
-    #     # )
-
-    #     # # More fields attached
-    #     # data["url"] = blob.media_link
-    #     self.logger.warning(data)
-
-    #     # # uncomment this line
-    #     # # blob.make_public() # Permissions are really messed up idk -- error : google.api_core.exceptions.BadRequest: 400 GET https://storage.googleapis.com/storage/v1/b/partyscene/o/file/acl?prettyPrint=false: Cannot get legacy ACL for an object when uniform bucket-level access is enabled. Read more at https://cloud.google.com/storage/docs/uniform-bucket-level-acces
-    #     result = await self.__media_handler.create_media_metadata(data)
-    #     # Refactor this return
-    #     status_code = HTTPStatus.CREATED
-    #     return jsonify(data=result, message="Media metadata created successfully.", status=status_code.phrase), status_code
-
     @route("/media/sign", methods=["GET"])
     # @cached(ttl=60 * 60 * 72) # Caching handled manually with Redis below
     async def sign(self):
